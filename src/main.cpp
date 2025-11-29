2692182236c45cc5e0fd1199070ad19be9f7c0e9
D Chinmay <zchinmayz@gmail.com>
2025-11-29 18:21:54 +0530

    type

--- src/main.cpp
+++ src/main.cpp
@@ -11,6 +11,15 @@ int main() {
     std::string in;
     std::getline(std::cin, in);
     if(in == "exit") break;
+    else if(in.find("type") != std::string::npos){
+      std::string tpin = in.substr(5);
+      if(tpin == "echo" || tpin == "exit" || tpin == "type"){
+        std::cout<<tpin<<" is a shell builtin"<<std::endl;
+      }
+      else{
+        std::cout<<tpin<<": not found"<<std::endl;
+      }
+    }
     else if(in.find("echo") != std::string::npos){
       std::cout<<in.substr(5)<<"\n";
     }
